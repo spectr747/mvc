92ceade5028aac59edab9befb851c5e544d31566
Spectr Spectrum <spectr747@gmail.com>
2019-11-06 19:38:08 +0300

    settings models

--- index.php
+++ index.php
@@ -10,9 +10,7 @@ error_reporting(E_ALL);
 // 2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
-
-// 3. Установка соединения с БД
-
+require_once(ROOT.'/components/Db.php');
 
 // 4. Вызов Router
 $router = new Router;
--- models/News.php
+++ models/News.php
@@ -1,8 +1,59 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+include_once ROOT . '/models/News.php';
+
+class News 
+{
+    
+    /**
+     * Returns single news item with specified id
+     * @param integer $id
+     */
+    public static function getNewsItemById($id)
+    {
+        $id = intval($id);
+        
+        if ($id) {
+            
+            $db = Db::getConnection();
+            
+            $result = $db->query('SELECT * from news WHERE id=' . $id);
+            
+#            $result->setFetchMode(PDO::FETCH_NUM);
+            $result->setFetchMode(PDO::FETCH_ASSOC);
+            
+            $newsItem = $result->fetch();
+            return $newsItem;
+        }
+    }
+    
+    /**
+     * Return single news item with specified id
+     * @param integer $id
+     */
+    public static function getNewsList()
+    {
+        $db = Db::getConnection();
+        
+        $newsList = array();
+        
+        $result = $db->query('SELECT id, title, date, short_content '
+                . 'FROM news '
+                . 'ORDER BY date DESC '
+                . 'LIMIT 10');
+        
+        $i = 0;
+        while($row = $result->fetch()) {
+            $newsList[$i]['id'] = $row['id'];
+            $newsList[$i]['title'] = $row['title'];
+            $newsList[$i]['date'] = $row['date'];
+            $newsList[$i]['short_content'] = $row['short_content'];
+            $i++;
+        }
+        
+        return $newsList;
+    }
+    
+    
+}
 
