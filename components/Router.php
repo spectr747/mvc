87604a8b622ec88eb1c0e7ffc4d705c550555da9
Spectr Spectrum <spectr747@gmail.com>
2019-11-06 15:31:01 +0300

    create and setting class Router

--- components/Router.php
+++ components/Router.php
@@ -6,11 +6,56 @@ class Router {
     
     public function __construct() 
     {
-        
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+    
+    /**
+     * Return request string
+     */
+    private function getURI() {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
     }
     
     public function run() 
     {
-        echo 'Class Router, method run';
+        // Получить строку запроса
+        $uri =  $this->getURI();
+        
+        // Проверить наличие такого запроса в routes.php
+        foreach ($this->routes as $uriPattern => $path) {
+            
+            // Сравниваем $uriPattern и $uri
+            if (preg_match("~$uriPattern~", $uri)) {
+                
+                // Определить какой контроллер
+                // и action обрабатывают запрос
+                $segments = explode('/', $path);
+                
+                $controllerName = ucfirst(array_shift($segments).'Controller');
+                $actionName = 'action'.ucfirst(array_shift($segments));
+                
+                // Подключить файл класса-контроллера
+                $controllerFile = ROOT . '/controllers/' . 
+                        $controllerName . '.php';
+
+                if (file_exists($controllerFile)) {
+                    include_once($controllerFile);
+                }
+
+                // Создать объект, вызвать метод (т.е. action)
+                $controllerObject = new $controllerName;
+                $result = $controllerObject->$actionName();
+                if ($result != null) {
+                    break;
+                }
+            }
+        }        
+        
+        
+        
+        
     }
 }
